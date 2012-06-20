92efd9394ea4cf013f8613c5fccdd68c3c4cc307
Ceci <ceci.tablas@gmail.com>
2012-06-20 12:05:00 -0300

    Adicao do enterprise

---
+++ Chronos/RegisterEnterprise.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Chronos
+{
+    public partial class RegisterEnterprise : System.Web.UI.Page
+    {
+       protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+        
+        
+        }
+
+        protected void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }   
+    }
+}
